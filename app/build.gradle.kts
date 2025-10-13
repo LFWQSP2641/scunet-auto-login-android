b7cb75a65127e6a41ae5ad6416930230763dcb31
LFWQSP2641 <liuyunbo2641@gmail.com>
2025-10-13 22:55:18 +0800

    Upgrade. v0.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.lfwqsp2641.safa"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
